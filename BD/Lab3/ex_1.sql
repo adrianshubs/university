6a6272c8a0f14c00fe4de1dfb6f69e7ef89d1d74
Adrian <adrianpetrusca18@outlook.com>
2025-03-30 16:28:30 +0300

    Update Lab3

--- BD/Lab3/ex_1.sql
+++ BD/Lab3/ex_1.sql
@@ -1,4 +1,4 @@
--- Sa se listeze toti studentii a caror nume de familie se termina cu litera 'u'.
+-- Se va lista toti studentii a caror nume de familie se termina cu litera 'u'.
 
 SELECT *
 FROM Student
