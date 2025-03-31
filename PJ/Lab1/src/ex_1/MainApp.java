3e8bfed5a527dc5be9041c8fa090467349163d43
adrianspage <adrianpetrusca18@outlook.com>
2025-04-01 01:24:16 +0300

    Update Lab1

--- PJ/Lab1/src/ex_1/MainApp.java
+++ PJ/Lab1/src/ex_1/MainApp.java
@@ -1,25 +1,32 @@
+/*
+ Se cere sa se scrie un program Java care sa calculeze si sa afiseze perimetrul si aria unui
+ dreptunghi. Valorile pentru lungime si latime se citesc de la tastatura.
+*/
+
 package ex_1;
 
-import java.util.Scanner;
+import java.util.Scanner; // Importam clasa Scanner pentru citirea de la tastatura
 
 public class MainApp {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in); // Cream un obiect Scanner pentru a citi de la tastatura
 
+        // Solicitam utilizatorului sa introduca lungimea si latimea dreptunghiului
         System.out.print("Lungimea = ");
         int lungimea = scanner.nextInt();
 
         System.out.print("Latimea = ");
         int latimea = scanner.nextInt();
 
-        // Calcularea perimetrului și a ariei
+        // Calculam perimetrul si aria dreptunghiului
         int perimetru = 2 * (lungimea + latimea);
         int aria = lungimea * latimea;
 
+        // Afisam rezultatele
         System.out.println("Perimetru = " + perimetru);
         System.out.println("Aria = " + aria);
 
-        scanner.close();
+        scanner.close(); // Inchidem scannerul pentru a evita scurgerile de resurse
     }
 }
